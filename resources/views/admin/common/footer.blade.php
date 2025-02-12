653cf7bff0884908e462ffa2dcb4a26c3637abe6
Tien Dung <dn0102302@gmail.com>
2025-02-12 17:13:03 +0700

    fix lỗi user admin login admin

--- resources/views/admin/common/footer.blade.php
+++ resources/views/admin/common/footer.blade.php
@@ -1,7 +1,6 @@
 <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
-        <b>Version</b> 3.0.5
+        <b>Version</b> 4.0.0
     </div>
-    <strong>Copyright &copy;2024 </strong> All rights
-    reserved.
+    <strong>2025 &copy; DungHotel</strong>
 </footer>
--- resources/views/admin/user/form.blade.php
+++ resources/views/admin/user/form.blade.php
@@ -5,35 +5,37 @@
             <div class="col-md-3">
                 <!-- Profile Image -->
                 <div class="card card-primary card-outline">
-                    <div class="card-body box-profile">
+                    <div class="card-body box-profile"style="padding-left: 10px; padding-right: 10px;">
                         <div class="text-center">
                             @if(isset($user) && !empty($user->avatar))
-                                <img src="{{ asset(pare_url_file($user->avatar)) }}" alt="" class=" margin-auto-div img-rounded profile-user-img img-fluid img-circle"  id="image_render" style="height: 150px; width:150px;">
+                            <img src="{{ asset(pare_url_file($user->avatar)) }}" alt="" class=" margin-auto-div img-rounded profile-user-img img-fluid img-circle" id="image_render" style="height: 150px; width:150px;">
                             @else
-                                <img alt="" class="margin-auto-div img-rounded profile-user-img img-fluid img-circle" src="{{ asset('admin/dist/img/avatar5.png') }}" id="image_render" style="height: 150px; width:150px;">
+                            <img alt="" class="margin-auto-div img-rounded profile-user-img img-fluid img-circle" src="{{ asset('admin/dist/img/avatar5.png') }}" id="image_render" style="height: 150px; width:150px;">
                             @endif
                         </div>
                         @if (isset($user->name))
-                            <h3 class="profile-username text-center">{{ $user->name }}</h3>
+                        <h3 class="profile-username text-center">{{ $user->name }}</h3>
                         @endif
                         @if (isset($user->email))
-                            <p class="text-muted text-center">{{ $user->email }}</p>
-                        @endif
-                        @if (isset($user->phone))
-                            <p class="text-muted text-center">{{ $user->phone }}</p>
-                        @endif
-                        @if (isset($user->userRole))
-                            <p class="text-muted text-center">{{ isset($user->userRole[0]) ? $user->userRole[0]->display_name : '' }}</p>
-                        @endif
+                                <p style="display: inline-block; ">Email: {{ $user->email }}</p>
+                            @endif
+                            @if (isset($user->phone))
+                                <p style="display: inline-block; ">Phone: {{ $user->phone }}</p>
+                            @endif
+                            @if (isset($user->userRole))
+                                <p style="display: inline-block;">Vai trò: {{ isset($user->userRole[0]) ? $user->userRole[0]->display_name : '' }}</p>
+                            @endif
                         <div class="form-group">
                             <div class="input-group input-file" name="images">
-                                    <span class="input-group-btn">
-                                        <button class="btn btn-default btn-choose" type="button">Chọn tệp</button>
-                                    </span>
-                                <input type="text" class="form-control" placeholder='Không có tệp nào ...'/>
+                                <span class="input-group-btn">
+                                    <button class="btn btn-default btn-choose" type="button">Chọn tệp</button>
+                                </span>
+                                <input type="text" class="form-control" placeholder='Không có tệp nào ...' />
                                 <span class="input-group-btn"></span>
                             </div>
-                            <span class="text-danger "><p class="mg-t-5">{{ $errors->first('images') }}</p></span>
+                            <span class="text-danger ">
+                                <p class="mg-t-5">{{ $errors->first('images') }}</p>
+                            </span>
 
                         </div>
                     </div>
@@ -41,7 +43,8 @@
                 </div>
                 <!-- /.card -->
             </div>
-            <?php //dd($errors) ?>
+            <?php //dd($errors) 
+            ?>
             <!-- /.col -->
             <div class="col-md-9">
                 <div class="card">
@@ -49,51 +52,61 @@
                         <div class="tab-content">
                             <div class="tab-pane active" id="settings">
                                 <div class="form-group row">
-                                    <label for="inputName" class="col-sm-2 col-form-label">Họ và tên  <sup class="title-sup">(*)</sup></label>
+                                    <label for="inputName" class="col-sm-2 col-form-label">Họ và tên <sup class="title-sup">(*)</sup></label>
                                     <div class="col-sm-10">
                                         <input type="text" class="form-control" id="inputName" placeholder="Họ và tên" name="name" value="{{old('name', isset($user->name) ? $user->name : '')}}">
-                                        <span class="text-danger "><p class="mg-t-5">{{ $errors->first('name') }}</p></span>
+                                        <span class="text-danger ">
+                                            <p class="mg-t-5">{{ $errors->first('name') }}</p>
+                                        </span>
                                     </div>
                                 </div>
                                 <div class="form-group row">
-                                    <label for="inputEmail" class="col-sm-2 col-form-label">Email  <sup class="title-sup">(*)</sup></label>
+                                    <label for="inputEmail" class="col-sm-2 col-form-label">Email <sup class="title-sup">(*)</sup></label>
                                     <div class="col-sm-10">
                                         <input type="email" class="form-control" id="inputEmail" placeholder="Email" name="email" value="{{old('email', isset($user->email) ? $user->email : '')}}">
-                                        <span class="text-danger "><p class="mg-t-5">{{ $errors->first('name') }}</p></span>
+                                        <span class="text-danger ">
+                                            <p class="mg-t-5">{{ $errors->first('name') }}</p>
+                                        </span>
                                     </div>
                                 </div>
                                 @if( !isset($user->password) && empty($user->password))
-                                    <div class="form-group row {{ $errors->has('password') ? 'has-error' : '' }}">
-                                        <label for="inputName2" class="col-sm-2 col-form-label">Mật khẩu  <sup class="title-sup">(*)</sup></label>
-                                        <div class="col-sm-10">
-                                            <input type="text" name="password" class="form-control" value="" id="exampleInputEmail1" placeholder="Mật khẩu">
-                                        </div>
-                                        <span class="text-danger"><p class="mg-t-5">{{ $errors->first('password') }}</p></span>
+                                <div class="form-group row {{ $errors->has('password') ? 'has-error' : '' }}">
+                                    <label for="inputName2" class="col-sm-2 col-form-label">Mật khẩu <sup class="title-sup">(*)</sup></label>
+                                    <div class="col-sm-10">
+                                        <input type="text" name="password" class="form-control" value="" id="exampleInputEmail1" placeholder="Mật khẩu">
                                     </div>
+                                    <span class="text-danger">
+                                        <p class="mg-t-5">{{ $errors->first('password') }}</p>
+                                    </span>
+                                </div>
                                 @endif
                                 <div class="form-group row">
-                                    <label for="inputName2" class="col-sm-2 col-form-label">Phone  <sup class="title-sup">(*)</sup></label>
+                                    <label for="inputName2" class="col-sm-2 col-form-label">Phone <sup class="title-sup">(*)</sup></label>
                                     <div class="col-sm-10">
                                         <input type="text" class="form-control" id="inputName2" placeholder="Phone" name="phone" value="{{old('phone', isset($user->phone) ? $user->phone : '')}}">
-                                        <span class="text-danger "><p class="mg-t-5">{{ $errors->first('phone') }}</p></span>
+                                        <span class="text-danger ">
+                                            <p class="mg-t-5">{{ $errors->first('phone') }}</p>
+                                        </span>
                                     </div>
                                 </div>
-                                <div class="form-group row" >
-                                    <label for="inputName2" class="col-sm-2 col-form-label">Vai trò  <sup class="title-sup">(*)</sup></label>
+                                <div class="form-group row">
+                                    <label for="inputName2" class="col-sm-2 col-form-label">Vai trò <sup class="title-sup">(*)</sup></label>
                                     <div class="col-sm-10">
                                         <select name="role" class="form-control">
                                             <option value="">Chọn vai trò</option>
                                             @if($roles)
-                                                @foreach($roles as $role)
-                                                    <option  {{old('role', isset($listRoleUser->role_id) ? $listRoleUser->role_id : '') == $role->id ? 'selected=selected' : '' }}  value="{{$role->id}}">{{$role->display_name}}</option>
-                                                @endforeach
+                                            @foreach($roles as $role)
+                                            <option {{old('role', isset($listRoleUser->role_id) ? $listRoleUser->role_id : '') == $role->id ? 'selected=selected' : '' }} value="{{$role->id}}">{{$role->display_name}}</option>
+                                            @endforeach
                                             @endif
                                         </select>
-                                        <span class="text-danger"><p class="mg-t-5">{{ $errors->first('role') }}</p></span>
+                                        <span class="text-danger">
+                                            <p class="mg-t-5">{{ $errors->first('role') }}</p>
+                                        </span>
                                     </div>
                                 </div>
                                 <div class="form-group row">
-                                    <label for="inputName2" class="col-sm-2 col-form-label">Trạng thái  </label>
+                                    <label for="inputName2" class="col-sm-2 col-form-label">Trạng thái </label>
                                     <div class="col-sm-10">
                                         <div class="icheck-primary d-inline">
                                             <input type="radio" id="radioPrimary1" name="status" value="1" {{ isset($user->status) && $user->status == 1 ? 'checked' : '' }}>
